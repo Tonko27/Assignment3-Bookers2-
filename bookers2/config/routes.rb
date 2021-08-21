8cec9affb5e46ca670677c7c6de06339a4cf294c
Tonko27 <tonkotsu4127@gmail.com>
2021-08-21 17:57:46 +0000

    [Update]Assignment4-10

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  
+  get "about" => "top#about" , as: "about"
+
+  devise_for :users, controllers: {
+  registrations: 'users/registrations',
+  confirmations: 'users/confirmations',
+}
+
+  resources :books
+  resources :users, only: [:edit, :update, :index, :show]
+  
+
 end
